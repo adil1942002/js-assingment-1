e5c191ba52b7ba6ce8687cc4099fe3ccb47c8a6b
adil abid <117192462+adil1942002@users.noreply.github.com>
2022-11-01 10:27:43 -0700

    Add files via upload

---
+++ js first/index.js
@@ -0,0 +1,45 @@
+ //window.alert ("open");
+var a = "adil";
+var b = "abid";
+ console.log (a); 
+ console.log (b);
+
+var c = 20;
+ var d = 10;
+ console.log(c * d);
+
+ var e = 20;
+ var f = 10;
+ console.log(e - f);
+
+ var g = 20;
+ var h = 10;
+ console.log(g / h);
+
+ var i = 20;
+ var j = 10;
+ console.log(i + j);
+
+ var ii = 20;
+ var jj = 10;
+ console.log(i, j);
+
+ var k = 100;
+ var l =k++;
+ console.log(k)
+ 
+ var m = 100;
+ var n =m--;
+ console.log(m)
+
+ var o = 100;
+ var p =++o;
+ console.log(o)
+
+ var q = 100;
+ var r =--q;
+ console.log(q)
+
+ var s =100;
+ var t =10;
+ console.log(s*t+s-t);
